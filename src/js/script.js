ff5e92a7b08c25bb933c742dfe3331e65a4e05bc
Piotr Pelc <piotrpelc83@gmail.com>
2024-11-05 18:32:02 +0100

    Exercise 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,50 @@
+'use strict';
 {
-    "use strict";
 
-    const booksListElement = document.querySelector('.books-list');
-    const bookTemplate = document.getElementById('template-book');
-    const books = dataSource.books;
-    const template = Handlebars.compile(bookTemplate.innerHTML);
+  const booksListElement = document.querySelector('.books-list');
+  const bookTemplate = document.getElementById('template-book');
+  const books = dataSource.books;
+  const template = Handlebars.compile(bookTemplate.innerHTML);
+  const favoriteBooks = [];
     
-    const render = () => {
-        for(const book of books){
-            const generatedHTML = template(book);
-            const element = utils.createDOMFromHTML(generatedHTML);
-            booksListElement.appendChild(element);
-        }
+  const render = () => {
+    for(const book of books){
+      const generatedHTML = template(book);
+      const element = utils.createDOMFromHTML(generatedHTML);
+      booksListElement.appendChild(element);
     }
-    
-   render();
+  };
+
+  const initActions = () => {
+    const covers = document.querySelectorAll('.book__image');
+
+    for(const cover of covers){
+      cover.addEventListener('click',(e) => {
+        e.preventDefault();
+      });
+
+      cover.addEventListener('dblclick',function(){
+        cover.classList.add('favorite');
+        const newFav = this.dataset.id;
+
+        if (isItNewFav(newFav)){
+          favoriteBooks.push(newFav);
+        }
+        console.log('favBooks: ', favoriteBooks);
+      });
+    }
+  };
+
+  const isItNewFav = (newFav) => {
+    let count = 0;
+    for(const fav of favoriteBooks){
+      if (newFav === fav){
+        count++;
+      }
+    }
+    return count === 0 ? true : false;
+  };
+
+  render();
+  initActions();
 }
